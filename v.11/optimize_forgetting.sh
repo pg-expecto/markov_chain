c1d566d2e05ca5ef11e59e2a37bd5a79a45a3545
Ринат <kznalp@yandex.ru>
2026-06-23 16:18:00 +0300

    Update optimize_forgetting.sh

--- v.11/optimize_forgetting.sh
+++ v.11/optimize_forgetting.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 2026 Ринат (pg_expecto)
+# Copyright 2026 Ринат (markov_chain)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
